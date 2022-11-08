2d5f15786777ec90638d5ace2676fc0dd2caed61
ethanrettinger <ethan.rettinger@greatoaks.com>
2022-11-08 14:03:00 -0500

    Code block - fin tmr

--- js/index.js
+++ js/index.js
@@ -4,19 +4,25 @@ document.querySelector('#hamburgerIcon').addEventListener('click', function () {
   document.querySelector('#hamburger-menu').classList.toggle('hidden');
 });
 
-document.querySelector('#exitIcon').addEventListener('click', function () {
+document.querySelector('#blurbg').addEventListener('click', function () {
   document.querySelector('#hamburger-menu').classList.toggle('hidden');
 });
 
-// add delay of about 1 second so the user doesnt accidentally scroll before it is ready
-setTimeout(function () {
-  document.querySelectorAll('.projects').forEach((project) => {
-    project.addEventListener('mouseover', function () {
-      project.scrollIntoView({
-        behavior: 'smooth',
-        block: 'center',
-        inline: 'center',
-      });
-    });
+// when #code is scrolled into view, animate it with class animate-slide-and-fade-in
+
+const code = document.querySelector('code');
+const codeObserver = new IntersectionObserver(function (entries, codeObserver) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('animate-slide-and-fade-in');
+      entry.target.classList.add('visible')
+      codeObserver.unobserve(entry.target);
+    }
   });
-}, 1000);
+}, {
+  threshold: 0.5
+});
+
+codeObserver.observe(code);
