0850163060ab2776836f746934a7ad8fb725c205
ethanrettinger <ethan.rettinger@greatoaks.com>
2022-11-08 08:34:35 -0500

    Project file changes + git file removals

--- js/index.js
+++ js/index.js
@@ -7,3 +7,16 @@ document.querySelector('#hamburgerIcon').addEventListener('click', function () {
 document.querySelector('#exitIcon').addEventListener('click', function () {
   document.querySelector('#hamburger-menu').classList.toggle('hidden');
 });
+
+// add delay of about 1 second so the user doesnt accidentally scroll before it is ready
+setTimeout(function () {
+  document.querySelectorAll('.projects').forEach((project) => {
+    project.addEventListener('mouseover', function () {
+      project.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+        inline: 'center',
+      });
+    });
+  });
+}, 1000);
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,20 @@ module.exports = {
             opacity: 1,
             transform: 'translateY(0)',
           },
+          type: {
+            // text typing effect
+            '0%': { width: '0ch' },
+            '5%, 10%': { width: '1ch' },
+            '15%, 20%': { width: '2ch' },
+            '25%, 30%': { width: '3ch' },
+            '35%, 40%': { width: '4ch' },
+            '45%, 50%': { width: '5ch' },
+            '55%, 60%': { width: '6ch' },
+            '65%, 70%': { width: '7ch' },
+            '75%, 80%': { width: '8ch' },
+            '85%, 90%': { width: '9ch' },
+            '95%, 100%': { width: '10ch' },
+          },
         },
         'slide-from-left': {
           '0%': {
@@ -53,6 +67,7 @@ module.exports = {
         'slide-and-fade-in': 'slide-and-fade-in 0.7s ease-in-out forwards',
         'slide-from-left': 'slide-from-left 0.7s ease-in-out',
         'slide-from-right': 'slide-from-right 0.9s ease-in-out',
+        type: 'type 1.8s ease-out .8s 1 normal both',
         hoverspin: 'hoverspin 1s ease',
       },
     },
@@ -67,6 +82,8 @@ module.exports = {
         'hover',
         'focus',
       ],
+      visibility: ['responsive', 'group-hover', 'focus-within', 'hover', 'focus'],
+      opacity: ['responsive', 'group-hover', 'focus-within', 'hover', 'focus'],
     },
   },
   plugins: [
